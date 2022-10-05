a92a8ec086aa2c3d68a1c39fd306bc23c20b6143
Keks <keks@htmlacademy.ru>
2022-10-06 00:25:28 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,34 +1,42 @@
-
-function getRandomIntInclusive(number1, number2) {
-  if (number1 < 0 || number2 < 0 ) {
-    return NaN;
+const isNegativeArgument = (value) => value < 0;
+const isNoFiniteNumber = (value) => !Number.isFinite(value);
+const isValidArguments = (minValue, maxValue) => {
+  if (isNegativeArgument(minValue)) {
+    throw new RangeError(`${minValue} меньше 0`);
   }
-
-  if (!Number.isFinite(number1) || !Number.isFinite(number2) ) {
-    return NaN;
+  if (isNegativeArgument(maxValue)) {
+    throw new RangeError(`${maxValue} меньше 0`);
   }
+  if (isNoFiniteNumber(minValue)) {
+    throw new RangeError(`Проверьте значение ${minValue} введеного аргумента`);
+  }
+  if (isNoFiniteNumber(maxValue)) {
+    throw new RangeError(`Проверьте значение ${maxValue} введеного аргумента`);
+  }
+};
 
-  let min = (number1 <= number2) ? number1 : number2;
-  let max = (number1 > number2) ? number1 : number2;
+const getRandomIntInclusive = (minValue, maxValue) => {
+  isValidArguments(minValue, maxValue);
+
+  let min = Math.min(minValue, maxValue);
+  let max = Math.max(minValue, maxValue);
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-function getRandomCoordinatesFloat(number1, number2, digits = 1) {
-  if (number1 < 0 || number2 < 0 || digits < 0) {
-    return NaN;
-  }
+const getRandomFloat = (minValue, maxValue, digits = 1) => {
+  isValidArguments(minValue, maxValue);
 
-  if (!Number.isFinite(number1) || !Number.isFinite(number2) || !Number.isInteger(digits)) {
-    return NaN;
-  }
-
-  const min = Math.min(number1,number2);
-  const max = Math.max(number1,number2);
+  const min = Math.min(minValue, maxValue);
+  const max = Math.max(minValue, maxValue);
   const res = Math.random() * (max - min) + min;
   return Number(res.toFixed(digits));
-}
+};
 
-getRandomIntInclusive(-1,1);
-getRandomCoordinatesFloat(0, 10, 1);
+try {
+  getRandomIntInclusive(-1, 1);
+  getRandomFloat(0, 10, 2);
+} catch (error) {
+  //
+}
