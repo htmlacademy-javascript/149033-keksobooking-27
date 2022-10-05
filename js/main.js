ad301b2792e90eacc9859ac03ce435a71b68bf1c
amsv <ammosovaleksey@ya.ru>
2022-10-05 20:35:50 +0300

    +isNegative

--- js/main.js
+++ js/main.js
@@ -1,34 +1,45 @@
 
-function getRandomIntInclusive(number1, number2) {
-  if (number1 < 0 || number2 < 0 ) {
+const getRandomIntInclusive = (minValue, maxValue) => {
+
+  const isNegative = (value) => value < 0;
+  if (isNegative(minValue)) {
+    throw new RangeError('Параметр должен меньше 0' ) ;
+  }
+  if (isNegative(maxValue)) {
+    throw new RangeError('Параметр должен меньше 0' ) ;
+  }
+
+
+  if (!Number.isFinite(minValue) || !Number.isFinite(maxValue)) {
     return NaN;
   }
 
-  if (!Number.isFinite(number1) || !Number.isFinite(number2) ) {
-    return NaN;
-  }
-
-  let min = (number1 <= number2) ? number1 : number2;
-  let max = (number1 > number2) ? number1 : number2;
+  let min = Math.min(minValue, maxValue);
+  let max = Math.max(minValue, maxValue);
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-function getRandomCoordinatesFloat(number1, number2, digits = 1) {
-  if (number1 < 0 || number2 < 0 || digits < 0) {
+const getRandomFloat = (minValue, maxValue, digits = 1) => {
+  if (minValue < 0 || maxValue < 0 || digits < 0) {
     return NaN;
   }
 
-  if (!Number.isFinite(number1) || !Number.isFinite(number2) || !Number.isInteger(digits)) {
+  if (
+    !Number.isFinite(minValue) ||
+    !Number.isFinite(maxValue) ||
+    !Number.isInteger(digits)
+  ) {
     return NaN;
   }
 
-  const min = Math.min(number1,number2);
-  const max = Math.max(number1,number2);
+  const min = Math.min(minValue, maxValue);
+  const max = Math.max(minValue, maxValue);
   const res = Math.random() * (max - min) + min;
   return Number(res.toFixed(digits));
-}
+};
 
-getRandomIntInclusive(-1,1);
-getRandomCoordinatesFloat(0, 10, 1);
+
+getRandomIntInclusive(-1, 1);
+getRandomFloat(0, 10, 2);
