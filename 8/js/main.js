e090e8faa5f985cf083359ba16d752325a221f47
Keks <keks@htmlacademy.ru>
2022-10-24 00:07:42 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,13 @@
+import {creatListAdvertisement} from './data.js';
+import './card.js';
+import {setAdFormOff, setAdFormOn} from './form.js';
+import {setMapFilterOff, setMapFilterOn} from './filter.js';
+creatListAdvertisement();
+
+setAdFormOn();
+setAdFormOff();
+
+setMapFilterOn();
+setMapFilterOff();
+
+
