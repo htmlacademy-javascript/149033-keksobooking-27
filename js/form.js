25c13b5c577ada07fde45428de717865812a127b
amsv <ammosovaleksey@ya.ru>
2022-10-25 20:15:57 +0300

    +

--- js/form.js
+++ js/form.js
@@ -1,6 +1,30 @@
-import {setAdFormOff, setAdFormOn} from './togglePageStstus.js';
+import { setAdFormOff, setAdFormOn } from './togglePageStstus.js';
 
-setAdFormOn();
-setAdFormOff();
+const adForm = document.querySelector('.ad-form');
 
-export {setAdFormOff, setAdFormOn};
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'form__element--invalid',
+  successClass: 'form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+});
+
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+
+pristine.addValidator(
+  adForm.querySelector('#title'),
+  validateTitle,
+  '11111111'
+);
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  pristine.validate();
+});
+
+//setAdFormOn();
+//setAdFormOff();
+
+export { setAdFormOff, setAdFormOn };
