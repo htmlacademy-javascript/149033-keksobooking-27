ca3d820a00da34f3936a16b2dc9433925017a8d8
amsv <ammosovaleksey@ya.ru>
2022-10-26 12:45:50 +0300

    +

--- js/form.js
+++ js/form.js
@@ -8,19 +8,42 @@ const pristine = new Pristine(adForm, {
   successClass: 'form__element--valid',
   errorTextParent: 'ad-form__element',
   errorTextTag: 'span',
+  errorTextClass: 'text-help'
 });
-
-const validateTitle = (value) => value.length >= 30 && value.length <= 100;
 
 pristine.addValidator(
   adForm.querySelector('#title'),
-  validateTitle,
-  '11111111'
+  (value) => value.length >= 30 && value.length <= 100,
+  'Введите от 30 до 100 символов',
+  2,
+  true
+);
+
+pristine.addValidator(
+  adForm.querySelector('#price'),
+  (value) => value >= 0 && value <= 100000,
+  'Введите число от 0 до 100000',
+  2,
+  true
+);
+
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+const roomNumberValue = parseInt(roomNumber.value);
+const capacityF = (value) => {
+  console.log(value);
+  return roomNumberValue >= parseInt(value.match(/[1-3]/))
+};
+pristine.addValidator(
+  capacity,
+  capacityF,
+  'Введите правильное поле',
+  2,
+  true
 );
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
   pristine.validate();
 });
 
